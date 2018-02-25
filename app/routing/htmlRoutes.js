e9204717382b974516120be6cc540425a1df7f28
Cristina Zhang <rami0141@umn.edu>
2018-02-25 11:28:45 -0600

    html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = function(app) {
+	// This will route the user to the survey page
+	// When the user hits the url survey, I want to show them survey.html
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+	// This will route the user to the home page
+}
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parse application/json
 app.use(bodyParser.json());
 
+// Including the html routes in the server file
+require('./app/routing/htmlRoutes.js')(app);
+
 
 app.listen(PORT, function() {
 	console.log("App is listening on PORT: " + PORT);
