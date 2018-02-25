bf41023f842e5bfe4e1f952bfa51fee19bbbada4
Cristina Zhang <rami0141@umn.edu>
2018-02-25 11:05:19 -0600

    more code

---
+++ server.js
@@ -0,0 +1,7 @@
+//Need to require express, body-parser and path
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Tell the server we are creating an express server
+var app = express();
